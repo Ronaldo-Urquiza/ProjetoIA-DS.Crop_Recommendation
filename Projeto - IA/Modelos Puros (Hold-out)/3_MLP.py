659f4ffc27b3acc44983ee43748cdd8d60cb451c
Ronaldo Urquiza <80072240+Ronaldo-Urquiza@users.noreply.github.com>
2024-05-28 23:02:24 -0300

    Implantação de modelos de classificação em hold-out

---
+++ Projeto - IA/Modelos Puros (Hold-out)/3_MLP.py
@@ -0,0 +1,57 @@
+import os
+from conversao_normalizacao import converte_normaliza # Importação do código de conversão e normalização
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.neural_network import MLPClassifier
+
+# Caminho para o dataset
+ds_path = os.path.join('..', 'Crop_Recommendation.csv')
+
+# Carregando e pré-processa os dados
+DS = converte_normaliza(ds_path) #conversão e normalização
+#print(DS) #Teste para saber se a importação do código de conversão e normalização deu certo
+
+# Determina o número de colunas
+columns = len(DS.columns)
+#print(columns)
+
+# Extrai a última coluna
+y = DS['Crop'] # extrai a última coluna, que é o label
+#print(y)
+
+# Extrai as características (todas as colunas exceto a última)
+X = DS.iloc[:, :-1]
+#print(X)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None, stratify=y) #Separa variáveis para treinamento (X_train,y_train) e variáveis para teste(X_test,y_test) através do split puxando o X e y de cima
+
+                                                    #tamanho de teste = 20% então treinamento = 80% (podes coloar train_size=0.8 mas já está implícito)
+
+                                                    #random_state, que controla a aleatoriedade na divisão dos dados, none = totalmente aleatório
+
+                                                    #80% das amostras pertencem à y_train e 20% pertencem à y_test, ao usar stratify=y, a função train_test_split tentará manter essa proporção de 80-20 entre as classes y_train, y_test.
+
+                                                    #X (colunas representando os atributos), y (coluna(s) representando o target/alvo)
+
+# Aqui estamos criando um modelo de classificação de rede neural usando MLPClassifier.
+# O modelo terá duas camadas ocultas, com 20 e 10 neurônios, respectivamente.
+# A função de ativação usada nas camadas ocultas será a tangente hiperbólica ('tanh').
+# Vamos permitir que o treinamento continue por um máximo de 6000 iterações.
+model = MLPClassifier(hidden_layer_sizes=(20, 10), activation='tanh', max_iter=6000)
+
+# Treina o modelo MLP com os dados de treinamento
+model.fit(X_train, y_train)
+
+# Predição e Resultados
+result = model.predict(X_test) #irá retornar uma DataFrame com os dados que o seu modelo tentou predizer (20% do dataframe)
+
+# Esse bloquinho é so para mostrar o resultado do quanto sua árvore foi eficiente -=-=-=-=-=-
+acc = metrics.accuracy_score(result, y_test) #obtém o resultado da comparação entre sua árvore e a classe real do dataframe
+show = round(acc * 100)  #transforma no valor visual de porcentagem
+print("\nMulti Layer Perceptron:\n")
+print("Acurácia: {}%".format(show))
+print("O que o modelo tentou predizer ->  ",list(result))
+print("O que o modelo era para predizer ->",list(y_test))
+# fim do bloquinho -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
---
+++ Projeto - IA/Modelos Puros (Hold-out)/5_SVM.py
@@ -0,0 +1,54 @@
+import os
+from conversao_normalizacao import converte_normaliza # Importação do código de conversão e normalização
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.svm import SVC
+
+# Caminho para o dataset
+ds_path = os.path.join('..', 'Crop_Recommendation.csv')
+
+# Carregando e pré-processa os dados
+DS = converte_normaliza(ds_path) #conversão e normalização
+#print(DS) #Teste para saber se a importação do código de conversão e normalização deu certo
+
+# Determina o número de colunas
+columns = len(DS.columns)
+#print(columns)
+
+# Extrai a última coluna
+y = DS['Crop'] # extrai a última coluna, que é o label
+#print(y)
+
+# Extrai as características (todas as colunas exceto a última)
+X = DS.iloc[:, :-1]
+#print(X)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None, stratify=y) #Separa variáveis para treinamento (X_train,y_train) e variáveis para teste(X_test,y_test) através do split puxando o X e y de cima
+
+                                                    #tamanho de teste = 20% então treinamento = 80% (podes coloar train_size=0.8 mas já está implícito)
+
+                                                    #random_state, que controla a aleatoriedade na divisão dos dados, none = totalmente aleatório
+
+                                                    #80% das amostras pertencem à y_train e 20% pertencem à y_test, ao usar stratify=y, a função train_test_split tentará manter essa proporção de 80-20 entre as classes y_train, y_test.
+
+                                                    #X (colunas representando os atributos), y (coluna(s) representando o target/alvo)
+
+# Cria uma instância do modelo Support Vector Classifier (SVC) com kernel radial (RBF)
+model = SVC(kernel='rbf')
+
+# Treina o modelo SVC Ccom os dados de treinamento
+model = model.fit(X_train, y_train)
+
+# Predição e Resultados
+result = model.predict(X_test) #irá retornar uma DataFrame com os dados que o seu modelo tentou predizer (20% do dataframe)
+
+# Esse bloquinho é so para mostrar o resultado do quanto sua árvore foi eficiente -=-=-=-=-=-
+acc = metrics.accuracy_score(result, y_test) #obtém o resultado da comparação entre sua árvore e a classe real do dataframe
+show = round(acc * 100)  #transforma no valor visual de porcentagem
+print("\nSuppor Vector Classifier:\n")
+print("Acurácia: {}%".format(show))
+print("O que o modelo tentou predizer ->  ",list(result))
+print("O que o modelo era para predizer ->",list(y_test))
+# fim do bloquinho -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
